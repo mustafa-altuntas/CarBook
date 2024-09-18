d181d049173ba4bb64561ed009cad6910bb7c4b5
pc <pc@DESKTOP-EL2OSRJ>
2024-09-18 15:37:01 +0300

    Add project files.

---
+++ Core/CarBook.Domain/Pricing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain
+{
+    public class Pricing
+    {
+        public int PricingId { get; set; }
+        public string Name { get; set; } // Yıllık - Aylık - Haftalık - Günlük
+        public List<CarPricing> CarPricings { get; set; }
+
+    }
+}
---
+++ Infrastructer/CarBook.Persistence/Context/CarBookContext.cs
@@ -0,0 +1,37 @@
+﻿using CarBook.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Persistence.Context
+{
+    public class CarBookContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=CarBookDB;User ID=sa;Password=Password1;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+        public DbSet<Aboud> Abouds { get; set; }
+        public DbSet<Banner> Banners { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarDescription> CarDescriptions { get; set; }
+        public DbSet<CarFeature> CarFeatures { get; set; }
+        public DbSet<CarPricing> CarPricings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<FooterAddress> FooterAddresses { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Pricing> Pricings { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+     
+
+    }
+}
