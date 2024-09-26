52578c67d4fed46019a6c30a77afcb8976ac414a
mustafa altuntaş <altun_tas@outlook.com>
2024-09-26 17:22:27 +0300

    .

---
+++ Presentation/CarBook.WebApi/Controllers/TestimonialController.cs
@@ -0,0 +1,64 @@
+﻿
+using CarBook.Aplication.Features.Mediator.Commands.TestimonialCommands;
+using CarBook.Aplication.Features.Mediator.Queries.TestimonialQueries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+
+
+        private readonly IMediator _mediator;
+
+        public TestimonialController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var values = await _mediator.Send(new GetTestimonialQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSocialMediaById(int id)
+        {
+            var value = await _mediator.Send(new GetTestimonialByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSocialMedia(CreateTestimonialCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Referans medya bilgisi oluşturuldu.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateSocialMedia(UpdateTestimonialCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Referans medya bilgisi güncelendi.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveSocialMedia(RemoveTestimonialCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Referans medya bilgisi silindi.");
+        }
+
+
+
+
+
+
+    }
+}
