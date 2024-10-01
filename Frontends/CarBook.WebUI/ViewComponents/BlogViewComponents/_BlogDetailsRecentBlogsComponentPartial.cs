dd796013d27bc87e89d725e283845c8537d97fe9
mustafa altuntaş <altun_tas@outlook.com>
2024-10-01 21:19:49 +0300

    .

--- Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsRecentBlogsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsRecentBlogsComponentPartial.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Carbook.DTO.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace CarBook.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailsRecentBlogsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailsRecentBlogsComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var resultMessage = await client.GetAsync("https://localhost:7112/api/Blog/GetLast3BlogsWhitAuthorsList");
+            if (resultMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resultMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<List<ResultLast3BlogsWhitAuthorsDto>>(jsonData);
+                return View(value);
+            }
+
             return View();
         }
     }
