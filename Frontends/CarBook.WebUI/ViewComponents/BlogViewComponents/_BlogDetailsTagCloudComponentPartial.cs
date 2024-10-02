b575dae5934c7dd1bce3561609752e18155cbfe6
mustafa altuntaş <altun_tas@outlook.com>
2024-10-02 17:04:40 +0300

    .

--- Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsTagCloudComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsTagCloudComponentPartial.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Carbook.DTO.TagCloudDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailsTagCloudComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailsTagCloudComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var resultMessage = await client.GetAsync("https://localhost:7112/api/TagCloud");
+            if (resultMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resultMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTagCloudDto>>(jsonData);
+                return View(values);
+            }
+
+
             return View();
         }
     }
---
+++ Frontends/Carbook.DTO/TagCloudDtos/ResultTagCloudDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.DTO.TagCloudDtos
+{
+    public class ResultTagCloudDto
+    {
+        public int TagCloudID { get; set; }
+        public string Title { get; set; }
+    }
+}
