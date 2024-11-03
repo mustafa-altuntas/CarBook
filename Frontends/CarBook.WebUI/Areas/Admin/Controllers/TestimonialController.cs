9aa53a3f40bb42d129b4d4944af88ab99d1705cc
mustafa altuntaş <altun_tas@outlook.com>
2024-11-03 13:50:23 +0300

    .

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/TestimonialController.cs
@@ -0,0 +1,106 @@
+﻿using Carbook.DTO.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TestimonialController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestimonialController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var resutlMessage = await client.GetAsync("https://localhost:7112/api/Testimonial");
+            if (resutlMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resutlMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateTestimonialDto createTestimonialDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createTestimonialDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var resultMessage = await client.PostAsync("https://localhost:7112/api/Testimonial", stringContent);
+            if (resultMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+                //return RedirectToAction("Index", "Brand", new { area = "Admin" });
+            }
+
+            return View(createTestimonialDto);
+        }
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var resultMessage = await client.DeleteAsync($"https://localhost:7112/api/Testimonial/{id}");
+            if (resultMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var resultMessage = await client.GetAsync($"https://localhost:7112/api/Testimonial/{id}");
+            if (resultMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resultMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<UpdateTestimonialDto>(jsonData);
+                TempData["UpdateTestimonialId"] = value.TestimonialId;
+                value.TestimonialId = 0;
+                return View(value);
+            }
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(UpdateTestimonialDto updateTestimonialDto)
+        {
+            updateTestimonialDto.TestimonialId = (int)TempData["UpdateTestimonialId"];
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateTestimonialDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var resultMessage = await client.PutAsync("https://localhost:7112/api/Testimonial", stringContent);
+            if (resultMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(updateTestimonialDto);
+
+        }
+
+    }
+}
