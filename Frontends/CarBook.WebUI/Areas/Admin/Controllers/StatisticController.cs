3f32c44f7516b73c7a686e75415b4c790a093794
mustafa altuntaş <altun_tas@outlook.com>
2024-11-03 14:33:25 +0300

    .

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/StatisticController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class StatisticController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
