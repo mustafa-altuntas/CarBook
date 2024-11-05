168a959da1643a34d41d83c63bf7c7495f14f7e8
mustafa altuntaş <altun_tas@outlook.com>
2024-11-05 12:38:23 +0300

    .

---
+++ Presentation/CarBook.WebApi/Controllers/RentACarController.cs
@@ -0,0 +1,28 @@
+﻿using CarBook.Aplication.Features.Mediator.Queries.RentACarQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentACarController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public RentACarController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RentACarListByLocation(GetRentACarQuery query)
+        {
+            var values = await mediator.Send(query);
+            return Ok(values);
+        }
+
+
+    }
+}
