c4ab2e2d81dc5e207fdf582f3284435e9e9844f5
mustafa altuntaş <altun_tas@outlook.com>
2024-11-06 20:11:21 +0300

    .

---
+++ Presentation/CarBook.WebApi/Controllers/ReservationController.cs
@@ -0,0 +1,28 @@
+﻿using CarBook.Aplication.Features.Mediator.Commands.ReservationCommands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+
+        private readonly IMediator mediator;
+
+        public ReservationController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateReservation(CreateReservationCommand command)
+        {
+            await mediator.Send(command);
+            return Ok("Rezervasyon bilgisi oluşturuldu.");
+        }
+
+    }
+}
