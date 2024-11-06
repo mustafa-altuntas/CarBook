c1dadd8ad270c2b862906fb80e7b0f6aee9e09aa
mustafa altuntaş <altun_tas@outlook.com>
2024-11-06 18:29:38 +0300

    .

---
+++ Core/CarBook.Aplication/Features/Mediator/Handlers/ReservationHandlers/CreateReservationCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using CarBook.Aplication.Features.Mediator.Commands.ReservationCommands;
+using CarBook.Aplication.Interfaces;
+using CarBook.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Aplication.Features.Mediator.Handlers.ReservationHandlers
+{
+    public class CreateReservationCommandHandler:IRequestHandler<CreateReservationCommand>
+    {
+        private readonly IRepository<Reservation> _repository;
+
+        public CreateReservationCommandHandler(IRepository<Reservation> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateReservationCommand request, CancellationToken cancellationToken)
+        {
+            var value = new Reservation()
+            {
+                Surname = request.Surname,
+                Name = request.Name,
+                Email = request.Email,
+                Phone = request.Phone,
+                DropOffLocationId = request.DropOffLocationId,
+                PickUpLocationId = request.PickUpLocationId,
+                CarId = request.CarId,
+                Age = request.Age,
+                DriverLicenseYear = request.DriverLicenseYear,
+                Description = request.Description
+            };
+
+            await _repository.CreateAsync(value);
+        }
+    }
+}
--- Frontends/CarBook.WebUI/Controllers/ReservationController.cs
+++ Frontends/CarBook.WebUI/Controllers/ReservationController.cs
@@ -1,13 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Carbook.DTO.LocationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.Controllers
 {
     public class ReservationController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ReservationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
         {
             ViewBag.v1 = "Araç Kiralama";
             ViewBag.v2 = "Araç Rezervasyon Formu";
+
+            var client = _httpClientFactory.CreateClient();
+            var resultMessage = await client.GetAsync($"https://localhost:7112/api/Location");
+            if(resultMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resultMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+
+                List<SelectListItem> selectListItems = values.Select(l => new SelectListItem { Text = l.Name, Value = l.Name }).ToList();
+                ViewBag.LocationSelectList = selectListItems;
+
+                return View();
+            }
+
+
+
+
+
 
             return View();
         }
