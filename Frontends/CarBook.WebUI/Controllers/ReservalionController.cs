fb40c2c51e299c1616bdc9ccc712b9b5016137f7
mustafa altuntaş <altun_tas@outlook.com>
2024-11-06 11:40:51 +0300

    .

---
+++ Frontends/CarBook.WebUI/Controllers/ReservalionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class ReservalionController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Araç Kiralama";
+            ViewBag.v2 = "Araç Rezervasyon Form";
+
+            return View();
+        }
+    }
+}
