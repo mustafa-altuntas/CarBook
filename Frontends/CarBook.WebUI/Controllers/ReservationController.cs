6c133b91c782c850b5611711618619f547fa1f21
mustafa altuntaş <altun_tas@outlook.com>
2024-11-06 21:06:49 +0300

    .

--- Frontends/CarBook.WebUI/Controllers/ReservationController.cs
+++ Frontends/CarBook.WebUI/Controllers/ReservationController.cs
@@ -1,7 +1,10 @@
 ﻿using Carbook.DTO.LocationDtos;
+using Carbook.DTO.ReservationDtos;
+using Carbook.DTO.TestimonialDtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace CarBook.WebUI.Controllers
 {
@@ -14,7 +17,8 @@ namespace CarBook.WebUI.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<IActionResult> Index()
+        [HttpGet]
+        public async Task<IActionResult> Index(int id)
         {
             ViewBag.v1 = "Araç Kiralama";
             ViewBag.v2 = "Araç Rezervasyon Formu";
@@ -32,12 +36,26 @@ namespace CarBook.WebUI.Controllers
                 return View();
             }
 
-
-
-
-
-
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateReservationDto dto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(dto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var resultMessage = await client.PostAsync("https://localhost:7112/api/Reservation", stringContent);
+            if (resultMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index),nameof(DefaultController));
+                //return RedirectToAction("Index", "Brand", new { area = "Admin" });
+            }
+
+            return View(dto);
+        }
+
+
+
     }
 }
