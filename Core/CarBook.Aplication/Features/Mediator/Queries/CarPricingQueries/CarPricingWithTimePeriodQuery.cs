bf06efefcc6d5de85fea716f94e88889c33b47f3
mustafa altuntaş <altun_tas@outlook.com>
2024-11-07 13:23:31 +0300

    .

---
+++ Core/CarBook.Aplication/Features/Mediator/Queries/CarPricingQueries/CarPricingWithTimePeriodQuery.cs
@@ -0,0 +1,15 @@
+﻿using CarBook.Aplication.Features.Mediator.Results.CarPricingResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Aplication.Features.Mediator.Queries.CarPricingQueries
+{
+    public class CarPricingWithTimePeriodQuery:IRequest<List<GerCarPricingWithTimePeriodQueryResult>>
+    {
+
+    }
+}
