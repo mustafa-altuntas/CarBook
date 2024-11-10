c1a9ebfa3da0721a2fe24f74bb4887218dcf64a1
mustafa altuntaş <altun_tas@outlook.com>
2024-11-10 11:21:25 +0300

    .

--- Frontends/CarBook.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutViewComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutViewComponentPartial.cs
@@ -1,11 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Carbook.DTO.AboutDtos;
+using Carbook.DTO.FooterAddressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _FooterUILayoutViewComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterUILayoutViewComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var resutlMessage = await client.GetAsync("https://localhost:7112/api/https://localhost:7112/api/FooterAddress");
+            if (resutlMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resutlMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAddresDto>>(jsonData);
+                var value = values.FirstOrDefault();
+                return View(value);
+            }
+
+
             return View();
         }
     }
