6b977c487b400b72881e819c5aaeb8a4e3f01e6e
mustafa altuntaş <altun_tas@outlook.com>
2024-11-10 21:11:16 +0300

    .

--- Frontends/CarBook.WebUI/ViewComponents/CommentViewComponent/_CommentListByBlogComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/CommentViewComponent/_CommentListByBlogComponentPartial.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Carbook.DTO.CommentDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.CommentViewComponent
 {
     public class _CommentListByBlogComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CommentListByBlogComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7112/api/Comment/CommentListByBlog?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<List<ResutlCommentDto>>(jsonData);
+                return View(value);
+            }
+
             return View();
         }
+
+
     }
 }
