876ced93e68349c9794cd8d17746c56ebf7e9ae6
mustafa altuntaş <altun_tas@outlook.com>
2024-11-11 16:40:17 +0300

    .

---
+++ Core/CarBook.Aplication/Features/Mediator/Commands/CommentCommands/CreateCommentCommand.cs
@@ -0,0 +1,20 @@
+﻿using CarBook.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Aplication.Features.Mediator.Commands.CommentCommands
+{
+    public class CreateCommentCommand:IRequest
+    {
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Description { get; set; }
+        public int BlogID { get; set; }
+
+
+    }
+}
