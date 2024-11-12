187cd40e8240328367befac0493d002bdb6d3762
mustafa altuntaş <altun_tas@outlook.com>
2024-11-12 10:30:18 +0300

    .

--- Frontends/Carbook.DTO/CommentDtos/CreateCommentDto.cs
+++ Frontends/Carbook.DTO/CommentDtos/CreateCommentDto.cs
@@ -11,6 +11,7 @@ namespace Carbook.DTO.CommentDtos
         public string Name { get; set; }
         public DateTime CreatedDate { get; set; }
         public string Description { get; set; }
+        public string Email { get; set; }
         public int BlogID { get; set; }
     }
 }
