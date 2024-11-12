0fef03c8a4867a327a3d0f9501cbfc64c97653f2
mustafa altuntaş <altun_tas@outlook.com>
2024-11-12 20:13:28 +0300

    .

---
+++ Infrastructer/CarBook.Persistence/Repositories/CarFeatureRepositories/CarFeatureRepository.cs
@@ -0,0 +1,27 @@
+﻿using CarBook.Aplication.Interfaces.CarFeatureInterfaces;
+using CarBook.Domain;
+using CarBook.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Persistence.Repositories.CarFeatureRepositories
+{
+    public class CarFeatureRepository : ICarFeatureRepository
+    {
+        private readonly CarBookContext _context;
+
+        public CarFeatureRepository(CarBookContext context)
+        {
+            _context = context;
+        }
+
+        public List<CarFeature> GetCarFeatureByCarId(int carId)
+        {
+            var values = _context.CarFeatures.Where(x=>x.CarId == carId).ToList();
+            return values;
+        }
+    }
+}
