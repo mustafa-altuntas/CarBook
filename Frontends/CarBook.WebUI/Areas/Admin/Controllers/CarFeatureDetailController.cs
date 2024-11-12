043ba52fd907f93af6cd4494dc9312c15238cef1
mustafa altuntaş <altun_tas@outlook.com>
2024-11-12 14:02:35 +0300

    .

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/CarFeatureDetailController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CarFeatureDetailController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
