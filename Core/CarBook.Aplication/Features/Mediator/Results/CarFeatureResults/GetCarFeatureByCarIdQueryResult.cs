b9985ec72d522c33928edd8cab3cf2b86cb45ffd
mustafa altuntaş <altun_tas@outlook.com>
2024-11-12 15:16:56 +0300

    .

---
+++ Core/CarBook.Aplication/Features/Mediator/Results/CarFeatureResults/GetCarFeatureByCarIdQueryResult.cs
@@ -0,0 +1,19 @@
+﻿using CarBook.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Aplication.Features.Mediator.Results.CarFeatureResults
+{
+    public class GetCarFeatureByCarIdQueryResult
+    {
+        public int CarFeatureId { get; set; }
+        public int CarId { get; set; }
+        public int FeatureId { get; set; }
+        public string FeatureName { get; set; }
+
+        public bool Available { get; set; }
+    }
+}
