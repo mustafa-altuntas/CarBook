01ecc4c027cd1fc3479ad7a4ece158aa08b334f9
mustafa altuntaş <altun_tas@outlook.com>
2024-11-12 20:26:33 +0300

    .

--- Frontends/CarBook.WebUI/Areas/Admin/Controllers/CarFeatureDetailController.cs
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/CarFeatureDetailController.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Carbook.DTO.CarFeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CarFeatureDetailController : Controller
     {
-        public IActionResult Index(int id)
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CarFeatureDetailController(IHttpClientFactory httpClientFactory)
         {
-            ViewBag.Id = id;
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IActionResult> Index(int carId)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var resultMessage = await client.GetAsync($"https://localhost:7112/api/CarFeature/{carId}");
+            if(resultMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resultMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarFeatureByCarIdDto>>(jsonData);
+                return View(values);
+            }
+
+            ViewBag.Id = carId;
             return View();
         }
     }
