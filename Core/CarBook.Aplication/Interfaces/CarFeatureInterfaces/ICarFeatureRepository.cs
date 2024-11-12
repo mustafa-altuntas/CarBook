aa20cc9790321b0fddec4dfff306446630a33df1
mustafa altuntaş <altun_tas@outlook.com>
2024-11-12 20:13:22 +0300

    .

---
+++ Core/CarBook.Aplication/Interfaces/CarFeatureInterfaces/ICarFeatureRepository.cs
@@ -0,0 +1,14 @@
+﻿using CarBook.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Aplication.Interfaces.CarFeatureInterfaces
+{
+    public interface ICarFeatureRepository
+    {
+        List<CarFeature> GetCarFeatureByCarId(int carId);
+    }
+}
