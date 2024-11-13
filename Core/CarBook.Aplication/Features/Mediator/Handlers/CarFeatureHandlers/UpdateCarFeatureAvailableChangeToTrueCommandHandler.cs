d143238b53b249c00c0f51373a4f1131ecbba61d
mustafa altuntaş <altun_tas@outlook.com>
2024-11-13 10:51:08 +0300

    .

---
+++ Core/CarBook.Aplication/Features/Mediator/Handlers/CarFeatureHandlers/UpdateCarFeatureAvailableChangeToTrueCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using CarBook.Aplication.Features.Mediator.Commands.CarFeatureCommands;
+using CarBook.Aplication.Interfaces.CarFeatureInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Aplication.Features.Mediator.Handlers.CarFeatureHandlers
+{
+    public class UpdateCarFeatureAvailableChangeToTrueCommandHandler : IRequestHandler<UpdateCarFeatureAvailableChangeToTrueCommand>
+    {
+        private readonly ICarFeatureRepository _repository;
+
+        public UpdateCarFeatureAvailableChangeToTrueCommandHandler(ICarFeatureRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateCarFeatureAvailableChangeToTrueCommand request, CancellationToken cancellationToken)
+        {
+            _repository.ChangeCarFeatureAvailableToTrue(request.Id);
+        }
+    }
+}
