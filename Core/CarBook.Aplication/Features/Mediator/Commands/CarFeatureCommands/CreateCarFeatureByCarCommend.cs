d4943ecda6d94a6b5dd7c4e2e80aa1b6be519061
mustafa altuntaş <altun_tas@outlook.com>
2024-11-13 15:22:35 +0300

    .

---
+++ Core/CarBook.Aplication/Features/Mediator/Commands/CarFeatureCommands/CreateCarFeatureByCarCommend.cs
@@ -0,0 +1,17 @@
+﻿using CarBook.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Aplication.Features.Mediator.Commands.CarFeatureCommands
+{
+    public class CreateCarFeatureByCarCommend:IRequest
+    {
+        public int CarId { get; set; }
+        public int FeatureId { get; set; }
+        public bool Available { get; set; }
+    }
+}
