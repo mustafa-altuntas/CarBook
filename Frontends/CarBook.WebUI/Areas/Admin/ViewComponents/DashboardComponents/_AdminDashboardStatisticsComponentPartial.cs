459569bb659a0df66ddc31432809dd0133c7970f
mustafa altuntaş <altun_tas@outlook.com>
2024-11-13 23:30:28 +0300

    .

---
+++ Frontends/CarBook.WebUI/Areas/Admin/ViewComponents/DashboardComponents/_AdminDashboardStatisticsComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using Carbook.DTO.StatisticDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Areas.Admin.ViewComponents.DashboardComponents
+{
+    public class _AdminDashboardStatisticsComponentPartial:ViewComponent
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AdminDashboardStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var resultMessage = await client.GetAsync($"https://localhost:7112/api/Statistic/GetAllStatistics");
+            if (resultMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resultMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultAllStatisticDto>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
