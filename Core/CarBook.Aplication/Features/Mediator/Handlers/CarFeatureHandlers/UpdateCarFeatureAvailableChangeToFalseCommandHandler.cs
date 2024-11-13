f9e643ff29feb85856b5eda6c989829cd20869fe
mustafa altuntaş <altun_tas@outlook.com>
2024-11-13 10:49:29 +0300

    .

---
+++ Core/CarBook.Aplication/Features/Mediator/Handlers/CarFeatureHandlers/UpdateCarFeatureAvailableChangeToFalseCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using CarBook.Aplication.Features.Mediator.Commands.CarFeatureCommands;
+using CarBook.Aplication.Interfaces.CarFeatureInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Aplication.Features.Mediator.Handlers.CarFeatureHandlers
+{
+    public class UpdateCarFeatureAvailableChangeToFalseCommandHandler : IRequestHandler<UpdateCarFeatureAvailableChangeToFalseCommand>
+    {
+        private readonly ICarFeatureRepository _repository;
+
+        public UpdateCarFeatureAvailableChangeToFalseCommandHandler(ICarFeatureRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateCarFeatureAvailableChangeToFalseCommand request, CancellationToken cancellationToken)
+        {
+            _repository.ChangeCarFeatureAvailableToFalse(request.Id);
+        }
+    }
+}
