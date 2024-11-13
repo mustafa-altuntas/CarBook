aa8bc2ead22eed7256c97c7072764251cb111301
mustafa altuntaş <altun_tas@outlook.com>
2024-11-13 15:26:46 +0300

    .

---
+++ Core/CarBook.Aplication/Features/Mediator/Handlers/CarFeatureHandlers/CreateCarFeatureByCarCommendHandler.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.Aplication.Features.Mediator.Commands.CarFeatureCommands;
+using CarBook.Aplication.Interfaces.CarFeatureInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Aplication.Features.Mediator.Handlers.CarFeatureHandlers
+{
+    public class CreateCarFeatureByCarCommendHandler : IRequestHandler<CreateCarFeatureByCarCommend>
+    {
+        private readonly ICarFeatureRepository _repository;
+
+        public CreateCarFeatureByCarCommendHandler(ICarFeatureRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateCarFeatureByCarCommend request, CancellationToken cancellationToken)
+        {
+            _repository.CreateCarFeatureByCar(new Domain.CarFeature
+            {
+                Available = false,
+                CarId = request.CarId,
+                FeatureId = request.FeatureId,
+            });
+        }
+    }
+}
