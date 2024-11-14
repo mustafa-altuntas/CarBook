8cef64f4a50e5766723feffefe43e9d90de94f21
mustafa altuntaş <altun_tas@outlook.com>
2024-11-14 12:07:05 +0300

    .

---
+++ Frontends/CarBook.WebUI/Areas/Admin/ViewComponents/DashboardComponents/_DashboardCarPricingListComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Carbook.DTO.CarPricingDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Areas.Admin.ViewComponents.DashboardComponents
+{
+    public class _DashboardCarPricingListComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardCarPricingListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var resultMessage = await client.GetAsync($"https://localhost:7112/api/CarPricing/GetCarPricingWithTimePeriod");
+            if (resultMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resultMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarPricingWithTimePeriodDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+    }
+}
