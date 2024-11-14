fb99639c45fab230db766d359da67229757d49ca
mustafa altuntaş <altun_tas@outlook.com>
2024-11-14 20:59:36 +0300

    .

---
+++ Frontends/CarBook.WebUI/Areas/Admin/ViewComponents/DashboardComponents/_DashboardChart2ComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Areas.Admin.ViewComponents.DashboardComponents
+{
+    public class _DashboardChart2ComponentPartial: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
