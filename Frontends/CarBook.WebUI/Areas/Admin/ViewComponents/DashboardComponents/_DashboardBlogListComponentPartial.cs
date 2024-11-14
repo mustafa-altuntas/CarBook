dc9228ef09e249d99a7e7e164de532dd32cf696a
mustafa altuntaş <altun_tas@outlook.com>
2024-11-14 16:20:37 +0300

    .

---
+++ Frontends/CarBook.WebUI/Areas/Admin/ViewComponents/DashboardComponents/_DashboardBlogListComponentPartial.cs
@@ -0,0 +1,32 @@
+﻿using Carbook.DTO.BlogDtos;
+using Carbook.DTO.CarPricingDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Areas.Admin.ViewComponents.DashboardComponents
+{
+    public class _DashboardBlogListComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardBlogListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var resutlMessage = await client.GetAsync("https://localhost:7112/api/Blog/GetAllBlogsWithAuthorList");
+            if (resutlMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resutlMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAllBlogsWithAuthorDto>>(jsonData);
+                var result = values.OrderByDescending(b => b.CreatedDate).Take(5).ToList();
+                return View(result);
+            }
+            return View();
+        }
+
+    }
+}
