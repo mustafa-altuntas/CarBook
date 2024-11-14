158447656e0a5ee3febc4ffa937ec9b5f882e149
mustafa altuntaş <altun_tas@outlook.com>
2024-11-14 21:03:45 +0300

    .

---
+++ Frontends/CarBook.WebUI/Areas/Admin/ViewComponents/DashboardComponents/_DashboardChart3ComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Areas.Admin.ViewComponents.DashboardComponents
+{
+    public class _DashboardChart3ComponentPartial: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
