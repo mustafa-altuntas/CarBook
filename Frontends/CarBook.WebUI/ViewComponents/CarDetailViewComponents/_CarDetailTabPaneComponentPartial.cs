97651aa248ea12b8350054d367d8f0bdb92e69ad
mustafa altuntaş <altun_tas@outlook.com>
2024-11-15 15:35:47 +0300

    .

---
+++ Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailTabPaneComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Carbook.DTO.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.CarDetailViewComponents
+{
+	public class _CarDetailTabPaneComponentPartial : ViewComponent
+	{
+	
+		public async Task<IViewComponentResult> InvokeAsync(int carId)
+		{
+			return View();
+		}
+	}
+}
