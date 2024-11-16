1b13dc2b190d4625bcdf055f852a22f72bcd4f54
mustafa altuntaş <altun_tas@outlook.com>
2024-11-16 18:13:15 +0300

    .

---
+++ Presentation/CarBook.WebApi/Controllers/CarDescriptionController.cs
@@ -0,0 +1,25 @@
+﻿using CarBook.Aplication.Features.Mediator.Queries.CarDescriptionQueries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarDescriptionController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public CarDescriptionController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CarDescriptionByCarId(int carId)
+        {
+            var values = await _mediator.Send(new GetCarDescriptionByCarIdQuery(carId));
+            return Ok(values);
+        }
+    }
+}
