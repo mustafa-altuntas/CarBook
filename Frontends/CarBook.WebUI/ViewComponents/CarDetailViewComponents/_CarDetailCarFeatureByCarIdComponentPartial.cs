eea7930cd69ce1ca73a3cba5cd871e1e979f7974
mustafa altuntaş <altun_tas@outlook.com>
2024-11-16 17:32:32 +0300

    .

---
+++ Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailCarFeatureByCarIdComponentPartial.cs
@@ -0,0 +1,33 @@
+﻿using Carbook.DTO.CarFeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace CarBook.WebUI.ViewComponents.CarDetailViewComponents
+{
+	public class _CarDetailCarFeatureByCarIdComponentPartial : ViewComponent
+	{
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CarDetailCarFeatureByCarIdComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int carId)
+		{
+            ViewBag.Id = carId;
+
+            var client = _httpClientFactory.CreateClient();
+            var resultMessage = await client.GetAsync($"https://localhost:7112/api/CarFeature/{carId}");
+            if (resultMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resultMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarFeatureByCarIdDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+	}
+}
