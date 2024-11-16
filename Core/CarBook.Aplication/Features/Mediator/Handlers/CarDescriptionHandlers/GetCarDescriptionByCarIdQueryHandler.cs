73d574bbf5b141e4ec6b2f2c8a265f162103c29e
mustafa altuntaş <altun_tas@outlook.com>
2024-11-16 18:08:00 +0300

    .

---
+++ Core/CarBook.Aplication/Features/Mediator/Handlers/CarDescriptionHandlers/GetCarDescriptionByCarIdQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using CarBook.Aplication.Features.Mediator.Queries.CarDescriptionQueries;
+using CarBook.Aplication.Features.Mediator.Results.CarDescriptionResults;
+using CarBook.Aplication.Interfaces;
+using CarBook.Aplication.Interfaces.CarDescriptionInterfaces;
+using CarBook.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Aplication.Features.Mediator.Handlers.CarDescriptionHandlers
+{
+    public class GetCarDescriptionByCarIdQueryHandler : IRequestHandler<GetCarDescriptionByCarIdQuery, GetCarDescriptionByCarIdQueryResult>
+    {
+
+        private readonly ICarDescriptionRepository _repository;
+
+        public GetCarDescriptionByCarIdQueryHandler(ICarDescriptionRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetCarDescriptionByCarIdQueryResult> Handle(GetCarDescriptionByCarIdQuery request, CancellationToken cancellationToken)
+        {
+            var value = _repository.GetCarDescription(request.CarId);
+            return new GetCarDescriptionByCarIdQueryResult
+            {
+                CarId = value.CarId,
+                CarDescriptionId = value.CarDescriptionId,
+                Detail = value.Detail,
+            };
+        }
+    }
+}
