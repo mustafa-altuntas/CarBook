4532076a493b49bc3f03cb2255cd1e8a3fe5e81e
mustafa altuntaş <altun_tas@outlook.com>
2024-11-17 16:43:29 +0300

    .

---
+++ Infrastructer/CarBook.Persistence/Repositories/ReviewRepositories/ReviewRepository.cs
@@ -0,0 +1,27 @@
+﻿using CarBook.Aplication.Interfaces.ReviewInterfaces;
+using CarBook.Domain;
+using CarBook.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Persistence.Repositories.ReviewRepositories
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly CarBookContext _context;
+
+        public ReviewRepository(CarBookContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Review> GetReviewByCarId(int carId)
+        {
+            var values = _context.Reviews.Where(r=>r.CarId == carId).ToList();
+            return values;
+        }
+    }
+}
