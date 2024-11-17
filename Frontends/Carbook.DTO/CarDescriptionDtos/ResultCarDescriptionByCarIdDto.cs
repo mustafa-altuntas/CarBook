479abfadb014928dba619fa08c260498d08933fb
mustafa altuntaş <altun_tas@outlook.com>
2024-11-17 15:24:05 +0300

    .

---
+++ Frontends/Carbook.DTO/CarDescriptionDtos/ResultCarDescriptionByCarIdDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbook.DTO.CarDescriptionDtos
+{
+    public class ResultCarDescriptionByCarIdDto
+    {
+        public int CarDescriptionId { get; set; }
+        public int CarId { get; set; }
+        public string Detail { get; set; }
+    }
+}
