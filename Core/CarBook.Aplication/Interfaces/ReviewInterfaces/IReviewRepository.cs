0687248844e975868b644ac370f7d3ad621bb29f
mustafa altuntaş <altun_tas@outlook.com>
2024-11-17 16:43:23 +0300

    .

---
+++ Core/CarBook.Aplication/Interfaces/ReviewInterfaces/IReviewRepository.cs
@@ -0,0 +1,14 @@
+﻿using CarBook.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Aplication.Interfaces.ReviewInterfaces
+{
+    public interface IReviewRepository
+    {
+        IEnumerable<Review> GetReviewByCarId(int carId);
+    }
+}
