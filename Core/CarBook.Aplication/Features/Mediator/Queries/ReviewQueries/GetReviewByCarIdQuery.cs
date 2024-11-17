379310b931ed53d9bcf6995bfde8ebe4f8aaf3ac
mustafa altuntaş <altun_tas@outlook.com>
2024-11-17 16:47:08 +0300

    .

---
+++ Core/CarBook.Aplication/Features/Mediator/Queries/ReviewQueries/GetReviewByCarIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using CarBook.Aplication.Features.Mediator.Results.ReviewResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Aplication.Features.Mediator.Queries.ReviewQueries
+{
+    public class GetReviewByCarIdQuery:IRequest<List<GetReviewByCarIdQueryResult>>
+    {
+        public int CarId { get; set; }
+
+        public GetReviewByCarIdQuery(int carId)
+        {
+            CarId = carId;
+        }
+    }
+}
