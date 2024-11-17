92e7dba719af30c708d888b5a99dd3aa370b9b8e
mustafa altuntaş <altun_tas@outlook.com>
2024-11-17 16:27:40 +0300

    .

---
+++ Core/CarBook.Domain/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerImage { get; set; }
+        public string Comment { get; set; }
+        public int RaytingValue { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public Car Car { get; set; }
+        public int CarId { get; set; }
+
+    }
+}
