8b4bdc1f6af434fed5a1e36f39a7b700ca043372
mustafa altuntaş <altun_tas@outlook.com>
2024-11-17 16:50:04 +0300

    .

---
+++ Presentation/CarBook.WebApi/Controllers/ReviewController.cs
@@ -0,0 +1,27 @@
+﻿using CarBook.Aplication.Features.Mediator.Queries.ReviewQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+
+        public ReviewController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> ReviewListByCarId(int carId)
+        {
+            var values = await _mediator.Send(new GetReviewByCarIdQuery(carId));
+            return Ok(values);
+        }
+
+    }
+}
