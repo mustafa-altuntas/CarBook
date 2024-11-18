9f627bea053fa08181669aa8cfdebe50e640593a
mustafa altuntaş <altun_tas@outlook.com>
2024-11-18 19:07:38 +0300

    .

---
+++ Presentation/CarBook.WebApi/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.Aplication.Features.Mediator.Commands.AppUserCommands;
+using CarBook.Aplication.Features.Mediator.Queries.AppUserQueries;
+using CarBook.Aplication.Tools;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+
+        private readonly IMediator mediator;
+
+        public RegisterController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost("CreateUser")]
+        public async Task<IActionResult> CreateUser(CreateAppUserCommand command)
+        {
+            await mediator.Send(command);
+
+            return Ok("Kullanıcı başarıyla oluşturuldu.");
+        }
+
+    }
+}
