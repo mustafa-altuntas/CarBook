c1ceb607b1b2de28ec634568d9fb4a81c2353ad7
mustafa altuntaş <altun_tas@outlook.com>
2024-11-18 10:41:19 +0300

    .

--- Core/CarBook.Aplication/Tools/JwtTokenDefaults.cs
+++ Core/CarBook.Aplication/Tools/JwtTokenDefaults.cs
@@ -8,6 +8,9 @@ namespace CarBook.Aplication.Tools
 {
     public class JwtTokenDefaults
     {
-
+        public const string ValidAudience = "https://localhost";
+        public const string ValidIssuer = "https://localhost";
+        public const string Key = "carbookcarbook01";
+        public const int Expire = 3;
     }
 }
