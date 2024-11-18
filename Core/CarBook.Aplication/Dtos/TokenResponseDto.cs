a77af950007eef3b54b12dac0457e67671685470
mustafa altuntaş <altun_tas@outlook.com>
2024-11-18 10:15:50 +0300

    .

---
+++ Core/CarBook.Aplication/Dtos/TokenResponseDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Aplication.Dtos
+{
+    public class TokenResponseDto
+    {
+        public TokenResponseDto(string toke, DateTime expireDate)
+        {
+            Toke = toke;
+            ExpireDate = expireDate;
+        }
+
+        public string Toke { get; set; }
+        public DateTime ExpireDate { get; set; }
+        
+    }
+}
