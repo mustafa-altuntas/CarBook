6a1583431bfd270beb3364c568af3c93051edd0b
mustafa altuntaş <altun_tas@outlook.com>
2024-11-18 09:11:52 +0300

    .

---
+++ Core/CarBook.Aplication/Interfaces/AppUserInterfaces/IAppUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using CarBook.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Aplication.Interfaces.AppUserInterfaces
+{
+    public interface IAppUserRepository
+    {
+        Task<AppUser> GetByFilterAsync(Expression<Func<AppUser, bool>> filterExpression);
+    }
+}
