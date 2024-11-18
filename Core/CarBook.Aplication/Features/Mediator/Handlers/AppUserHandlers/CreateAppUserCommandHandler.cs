0fcdef4079972febcbe5101d5a4c9fec4ee598ca
mustafa altuntaş <altun_tas@outlook.com>
2024-11-18 12:14:02 +0300

    .

---
+++ Core/CarBook.Aplication/Features/Mediator/Handlers/AppUserHandlers/CreateAppUserCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using CarBook.Aplication.Enums;
+using CarBook.Aplication.Features.Mediator.Commands.AppUserCommands;
+using CarBook.Aplication.Interfaces;
+using CarBook.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Aplication.Features.Mediator.Handlers.AppUserHandlers
+{
+    public class CreateAppUserCommandHandler : IRequestHandler<CreateAppUserCommand>
+    {
+        private readonly IRepository<AppUser> _repository;
+
+        public CreateAppUserCommandHandler(IRepository<AppUser> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateAppUserCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.CreateAsync(new AppUser
+            {
+                Password = request.Password,
+                UserName = request.UserName,
+                AppRoleId = (int) RolesType.Member
+            });
+        }
+    }
+}
