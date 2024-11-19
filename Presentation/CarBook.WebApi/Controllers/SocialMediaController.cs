f4f67075cc9df3b95d836ad8f6ccecc2dc9b8de2
mustafa altuntaş <altun_tas@outlook.com>
2024-11-19 12:55:16 +0300

    .

--- Presentation/CarBook.WebApi/Controllers/SocialMediaController.cs
+++ Presentation/CarBook.WebApi/Controllers/SocialMediaController.cs
@@ -2,10 +2,12 @@
 using CarBook.Aplication.Features.Mediator.Commands.SocialMediaCommands;
 using CarBook.Aplication.Features.Mediator.Queries.SocialMediaQueries;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarBook.WebApi.Controllers
 {
+    [Authorize(Roles = "Admin")]
     [Route("api/[controller]")]
     [ApiController]
     public class SocialMediaController : ControllerBase
