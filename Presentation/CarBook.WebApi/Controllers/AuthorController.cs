10d7c13c1edbc3c488d11a6e4b38eb76def6339f
mustafa altuntaş <altun_tas@outlook.com>
2024-11-19 12:53:43 +0300

    .

--- Presentation/CarBook.WebApi/Controllers/AuthorController.cs
+++ Presentation/CarBook.WebApi/Controllers/AuthorController.cs
@@ -2,10 +2,12 @@
 using CarBook.Aplication.Features.Mediator.Commands.AuthorCommands;
 using CarBook.Aplication.Features.Mediator.Queries.AuthorQueries;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarBook.WebApi.Controllers
 {
+    [Authorize(Roles = "Admin")]
     [Route("api/[controller]")]
     [ApiController]
     public class AuthorController : ControllerBase
