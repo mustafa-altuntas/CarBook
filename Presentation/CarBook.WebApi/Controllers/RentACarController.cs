c1bd195c9114271602b34561a96169f7da0ee408
mustafa altuntaş <altun_tas@outlook.com>
2024-11-19 12:54:55 +0300

    .

--- Presentation/CarBook.WebApi/Controllers/RentACarController.cs
+++ Presentation/CarBook.WebApi/Controllers/RentACarController.cs
@@ -1,10 +1,12 @@
 ﻿using CarBook.Aplication.Features.Mediator.Queries.RentACarQueries;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarBook.WebApi.Controllers
 {
+    [Authorize(Roles = "Admin")]
     [Route("api/[controller]")]
     [ApiController]
     public class RentACarController : ControllerBase
