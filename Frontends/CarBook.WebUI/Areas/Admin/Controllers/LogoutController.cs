0c134b9b45a6f56cd0eb6b3686912cd0c05ee907
mustafa altuntaş <altun_tas@outlook.com>
2024-11-19 16:27:47 +0300

    .

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/LogoutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    [Area("Admin")]
+    public class LogoutController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Dashboard");
+        }
+    }
+}
