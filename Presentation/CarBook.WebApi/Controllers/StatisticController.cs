12ceeca6662604798a93a6eaabc9281f82b52ac1
mustafa altuntaş <altun_tas@outlook.com>
2024-11-19 12:55:20 +0300

    .

--- Presentation/CarBook.WebApi/Controllers/StatisticController.cs
+++ Presentation/CarBook.WebApi/Controllers/StatisticController.cs
@@ -1,10 +1,12 @@
 ﻿using CarBook.Aplication.Features.Mediator.Queries.StatisticQueries;
 using CarBook.Aplication.Interfaces.StatisticInterfaces;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarBook.WebApi.Controllers
 {
+    [Authorize(Roles = "Admin")]
     [Route("api/[controller]")]
     [ApiController]
     public class StatisticController : ControllerBase
