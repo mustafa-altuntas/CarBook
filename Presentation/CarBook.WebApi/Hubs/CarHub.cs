0aec5b6145c2b08bbd82b90796ad950b8eb6b96f
@mustafa-altuntas <2altuntas9@gmail.com>
2024-11-27 12:13:03 +0300

    [feat] Implement CarHub logic to send car count via SignalR

--- Presentation/CarBook.WebApi/Hubs/CarHub.cs
+++ Presentation/CarBook.WebApi/Hubs/CarHub.cs
@@ -1,8 +1,31 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using CarBook.Domain;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace CarBook.WebApi.Hubs
 {
     public class CarHub : Hub
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CarHub(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task SendCarCount()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var resultMessage = await client.GetAsync("https://localhost:7112/api/Car");
+            if (resultMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resultMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<Car>>(jsonData)!.Count();
+                
+                await Clients.All.SendAsync("ReceiveCarCount",values);
+            }
+        }
+
     }
 }
